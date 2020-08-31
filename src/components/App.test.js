e00d81848416fb11d515ad9eebee09443920d52d
antoniojps <antonio.santos@mindera.com>
2020-08-31 10:19:50 +0100

    Refactor/Fix: Refactor and rework for more behaviour driven dev descriptions / Fix polluted state tests with afterEach

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,20 +5,32 @@ import toJson from "enzyme-to-json";
 
 const app = shallow(<App />)
 
-it('renders correctly', () => {
-  expect(toJson(app)).toMatchSnapshot()
+afterEach(() => {
+  app.setState({
+    gifts: []
+  })
 })
 
-it('initializes the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([])
-})
+describe('App', () => {
+  it('renders correctly', () => {
+    expect(toJson(app)).toMatchSnapshot()
+  })
 
-it('adds a new gift to `state`', () => {
-  app.find('.btn-add').simulate('click')
-  expect(app.state().gifts).toEqual([{ id: 1 }])
-})
+  it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([])
+  })
 
-it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click')
-  expect(app.find('.gift-list').children().length).toEqual(2)
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click')
+    })
+
+    it('adds a new gift to `state`', () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }])
+    })
+
+    it('adds a new gift to the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(1)
+    })
+  })
 })
